04bcab2eb2ff2e2a7cb35adafe8b59a9aeda7d6c
Bear Hagen <post@bearhagen.com>
2016-11-21 16:03:28 +0100

    Added config file

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+    require 'vendor/autoload.php';
+
+    use DigitalOceanV2\Adapter\BuzzAdapter;
+    use DigitalOceanV2\DigitalOceanV2;
+
+    // create an adapter with your access token which can be
+    // generated at https://cloud.digitalocean.com/settings/applications
+    $token = file_get_contents('../secret.cfg');
+    $adapter = new BuzzAdapter($token);
+
+    // create a digital ocean object with the previous adapter
+    $digitalocean = new DigitalOceanV2($adapter);
+
+    require_once 'public/_templates/head.php';
