d41436a236e471418092e0cb751c8c2d891a06dc
Bear Hagen <post@bearhagen.com>
2016-11-21 17:47:06 +0100

    Added error pages

---
+++ public/403.php
@@ -0,0 +1,13 @@
+<?php require_once('_templates/head.php'); ?>
+    <link rel="stylesheet" href="/_resources/css/index.css">
+</head>
+<body>
+    <header id="hjem" class="tabs active">
+        <div class="center-v-h center-align">
+            <h1>403</h1>
+        </div>
+    </header>
+    <?php require_once('_templates/nav.php'); ?>
+    <?php require_once('_templates/footer.php'); ?>
+</body>
+</html>
