21737f0a4a312bdfb2019bc5a9d9dbc8cb358bab
Bear Hagen <post@bearhagen.com>
2016-11-21 20:20:53 +0100

    Script for navigation dynamic styling

---
+++ public/_resources/js/global.js
@@ -0,0 +1,35 @@
+/* Bjørnar Hagen - 2016 */
+
+(function(window) {
+    window.onload = ready;
+
+    function ready() {
+        navigationScroll();
+    };
+
+    function navigationScroll() {
+        var nav = document.querySelector("#nav-main");
+        var navClasses = nav.getAttribute("class");
+        var isTransparent = true;
+        var delay = 300;
+        var delayer;
+
+        window.onscroll = function() {
+            clearTimeout(delayer);
+
+            delayer = setTimeout(function() {
+                var awayFromTop = (window.pageYOffset || document.documentElement.scrollTop)  - (document.documentElement.clientTop || 0);
+
+                if (awayFromTop > 50 && isTransparent) {
+                    delay *= 2;
+                    isTransparent = false;
+                    nav.setAttribute("class", navClasses.replace("transparent", ""));
+                } else if (awayFromTop < 50 && !isTransparent) {
+                    isTransparent = true;
+                    nav.setAttribute("class", navClasses);
+                    delay = 300;
+                }
+            }, delay);
+        }
+    }
+})(window);
